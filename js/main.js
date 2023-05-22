f1aaca32f24a24d002cd927589a12c85e83e0f81
Maxence SARI <maxence.sari11@gmail.com>
2023-05-23 00:08:19 +0200

    test

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".headerBloc--news", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+var swiper = new Swiper(".popularBlock--slider", {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    freeMode: true,
+
+});
